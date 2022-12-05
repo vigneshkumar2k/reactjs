f052ab80b1c3ff10c0180bd5406ecb9edb070bb0
vignesh kumar <sivavignesh1754511@gmail.com>
2022-12-05 09:08:02 +0530

    reactjs

---
+++ src/Weather.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './Weather.css'
+const api = {
+  key: "0f7648e3bb45f233b1b09cb1a8ce4157",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function Weatherfunc() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°c
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default Weatherfunc;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Register from './Register';
+import Weatherfunc from './Weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <div>
+      {/* <h1>REACT ROUTER</h1> */}
+      <nav class="navbar navbar-dark bg-dark">
+         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <Link class="navbar-brand">Navbar</Link>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <Link to='/' class="nav-link" >Home <span class="sr-only">(current)</span></Link>
+      </li>
+      <li class="nav-item">
+        <Link to='/register' class="nav-link">register</Link>
+      </li>
+      <li class="nav-item">
+        <Link to='/Weather' class="nav-link">weather</Link>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+</nav>
+    
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/Weather' element={<Weatherfunc />}></Route>
+      </Routes>
+    </div>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
